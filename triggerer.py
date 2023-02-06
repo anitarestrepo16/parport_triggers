f0d6575edff03ed3577aa921d48f6a58e3c2de3b
Anita <52679613+anitarestrepo16@users.noreply.github.com>
2023-02-06 10:02:53 -0600

    added set_trigger_labels method

--- triggerer.py
+++ triggerer.py
@@ -1,7 +1,30 @@
-class Triggerer:
 
-    def __init__(self, address):
+
+class Triggerer():
+    '''
+    Attributes:
+    - trigger_labels: 
+    Methods:
+    - 
+    '''
+
+    def __init__(self, address, trigger_types):
+        '''
+        trigger_types: list of strings indicating the flag labels desired
+        '''
         self.p = ParallelPort(address)
+        self.trigger_labels = 
+    
+    def set_trigger_labels(self, trigger_types):
+        '''
+        Map the trigger_types (list of strings with the labels for the flags) onto pin settings.
+        
+        '''
+        trigger_labels = {}
+        for trigger, index in enumerate(trigger_types):
+            trigger_labels[trigger] = index + 1
+        return trigger_labels
+            
 
     def send_trigger(self, value, duration = .002):
         self.p.setData(value)
